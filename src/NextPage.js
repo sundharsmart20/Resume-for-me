2a0c160ab1ae7d97fc41b49988d01637b5353e90
sundharsmart20 <89132033+sundharsmart20@users.noreply.github.com>
2021-12-04 10:49:51 +0530

    add flex

--- src/NextPage.js
+++ src/NextPage.js
@@ -9,7 +9,7 @@ const NextPage=()=>{
                 <p><span>Tools: </span>HTML,CSS,MYSQL</p>
             </div>
         <div style={{'position':'relative','top':'-60px'}} class="row justify-content-between">  
-            <p style={{'position':'absolute','top':'40px','border':'2px solid black','width':'230px','borderRadius':'20px 0 20px 0','textAlign':'center'}}>CO-CURRICULAR ACTIVITIES </p>
+            <p style={{'position':'absolute','display':'flex','top':'40px','border':'2px solid black','width':'230px','borderRadius':'20px 0 20px 0','textAlign':'center'}}>CO-CURRICULAR ACTIVITIES </p>
                 <ul  style={{'padding':'50px','position':'relative','top':'40px'}}>
                     <li>Presented a paper on “EFFICIENT CACHE SUPPORTED PATH PLANNING ON ROADS“at Coimbatore Institute of Technology </li>
                     <li>Presented a paper on “BLUE EYES TECHNOLOGY” at Mahendra Engineering College </li>
