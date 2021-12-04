758a57d6d1a52edbad6c6280f6f18e8df66693a2
sundharsmart20 <89132033+sundharsmart20@users.noreply.github.com>
2021-12-04 10:39:04 +0530

    add text

--- src/NextPage.js
+++ src/NextPage.js
@@ -9,8 +9,8 @@ const NextPage=()=>{
                 <p><span>Tools: </span>HTML,CSS,MYSQL</p>
             </div>
         <div style={{'position':'relative','top':'-60px'}} class="row justify-content-between">  
-            <p style={{'position':'relative','top':'40px','border':'2px solid black','width':'230px','borderRadius':'20px 0 20px 0','textAlign':'center'}}>CO-CURRICULAR ACTIVITIES </p>
-                <ul  style={{'padding':'50px'}}>
+            <p style={{'position':'absolute','top':'40px','border':'2px solid black','width':'230px','borderRadius':'20px 0 20px 0','textAlign':'center'}}>CO-CURRICULAR ACTIVITIES </p>
+                <ul  style={{'padding':'50px','position':'relative','top':'40px'}}>
                     <li>Presented a paper on “EFFICIENT CACHE SUPPORTED PATH PLANNING ON ROADS“at Coimbatore Institute of Technology </li>
                     <li>Presented a paper on “BLUE EYES TECHNOLOGY” at Mahendra Engineering College </li>
                     <li>Attended webinar on “Virtual & Augmented Reality in industry 4.0” at Velammal Engineering College </li>
